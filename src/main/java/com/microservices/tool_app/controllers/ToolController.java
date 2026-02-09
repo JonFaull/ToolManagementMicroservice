fc4e35bdb53cca415da14c397a99163cfc0abc3c
JonFaull <jonathan.faull@gmail.com>
2026-02-09 20:01:00 +0000

    Add comment to test CI pipeline.

--- src/main/java/com/microservices/tool_app/controllers/ToolController.java
+++ src/main/java/com/microservices/tool_app/controllers/ToolController.java
@@ -109,7 +109,7 @@ public class ToolController {
 
         boolean isDeleted = toolService.deleteTool(id);
 
-        //Test CI pipelinettttttt.
+        //Test CI pipelinettttttkkkkt.
 
         if (!isDeleted) {
             throw new ResourceNotFoundException("Tool not found with ID: " + id);
