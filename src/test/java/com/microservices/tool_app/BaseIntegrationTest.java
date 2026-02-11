00b6b4d47a77a0896ca752661ea092f7cd8a868e
JonFaull <jonathan.faull@gmail.com>
2026-02-11 20:44:28 +0000

    Add integration tests.

--- src/test/java/com/microservices/tool_app/BaseIntegrationTest.java
+++ src/test/java/com/microservices/tool_app/BaseIntegrationTest.java
@@ -3,7 +3,6 @@ package com.microservices.tool_app;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
--- src/test/java/com/microservices/tool_app/UserIntegrationTest.java
+++ src/test/java/com/microservices/tool_app/UserIntegrationTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
