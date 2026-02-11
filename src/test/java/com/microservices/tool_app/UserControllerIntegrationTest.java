5642694a5cf3ae82944c3b6550e8a28c2803dcc6
JonFaull <jonathan.faull@gmail.com>
2026-02-11 19:42:41 +0000

    Add integration tests.

--- src/test/java/com/microservices/tool_app/UserControllerIntegrationTest.java
+++ src/test/java/com/microservices/tool_app/UserControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
@@ -18,6 +19,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+
 class UserControllerIntegrationTest extends BaseIntegrationTest{
 
     @Autowired
