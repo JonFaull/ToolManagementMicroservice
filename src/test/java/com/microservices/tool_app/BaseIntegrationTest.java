e376ac53fee200a168e9e00da8af099156a8d17b
JonFaull <jonathan.faull@gmail.com>
2026-02-11 19:45:31 +0000

    Add integration tests.

--- src/test/java/com/microservices/tool_app/BaseIntegrationTest.java
+++ src/test/java/com/microservices/tool_app/BaseIntegrationTest.java
@@ -2,10 +2,12 @@ package com.microservices.tool_app;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 
 public abstract class BaseIntegrationTest {
 
